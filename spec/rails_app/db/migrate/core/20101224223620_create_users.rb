80fde0e4de896261c6c63d4f2acedf4d75c3fba1
Yusuke Ebihara <biibiebi1234@gmail.com>
2017-01-27 23:29:02 +0900

    Set the same constrants as lib/generators/sorcery/templates/migration/core.rb

--- spec/rails_app/db/migrate/core/20101224223620_create_users.rb
+++ spec/rails_app/db/migrate/core/20101224223620_create_users.rb
@@ -1,13 +1,15 @@
 class CreateUsers < ActiveRecord::CompatibleLegacyMigration.migration_class
   def self.up
     create_table :users do |t|
-      t.string :username,         null: false
-      t.string :email,            default: nil
+      t.string :username
+      t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
 
       t.timestamps null: false
     end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
   end
 
   def self.down

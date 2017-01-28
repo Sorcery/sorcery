842a630e3733d727ab10ef1bfc4677fa11c3cf92
Yusuke Ebihara <biibiebi1234@gmail.com>
2017-01-28 22:55:35 +0900

    Revert "Remove not null constraint from email in users."
    This reverts commit b28de94165ba868a0bafb45451aa1a020c8b5a13.

--- lib/generators/sorcery/templates/migration/core.rb
+++ lib/generators/sorcery/templates/migration/core.rb
@@ -1,7 +1,7 @@
 class SorceryCore < <%= migration_class_name %>
   def change
     create_table :<%= model_class_name.tableize %> do |t|
-      t.string :email
+      t.string :email,            :null => false
       t.string :username
       t.string :crypted_password
       t.string :salt

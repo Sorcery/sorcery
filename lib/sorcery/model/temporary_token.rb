7c5fee441e03779ee2888ecef317440cd10a17e7
Steven Hoffman <fustrate@me.com>
2017-03-23 20:54:40 -0700

    Resurrect block authentication (#41)
    * Resurrect block authentication
    
    Signed-off-by: Steven Hoffman <git@fustrate.com>
    
    * fix some stupid errors
    
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/sorcery/model/temporary_token.rb
+++ lib/sorcery/model/temporary_token.rb
@@ -16,13 +16,34 @@ module Sorcery
       end
 
       module ClassMethods
-        def load_from_token(token, token_attr_name, token_expiration_date_attr)
-          return nil if token.blank?
+        def load_from_token(token, token_attr_name, token_expiration_date_attr = nil, &block)
+          return token_response(failure: :invalid_token, &block) if token.blank?
+
           user = sorcery_adapter.find_by_token(token_attr_name, token)
-          if !user.blank? && !user.send(token_expiration_date_attr).nil?
-            return Time.now.in_time_zone < user.send(token_expiration_date_attr) ? user : nil
+
+          return token_response(failure: :user_not_found, &block) unless user
+
+          unless check_expiration_date(user, token_expiration_date_attr)
+            return token_response(user: user, failure: :token_expired, &block)
           end
-          user
+
+          token_response(user: user, return_value: user, &block)
+        end
+
+        protected
+
+        def check_expiration_date(user, token_expiration_date_attr)
+          return true unless token_expiration_date_attr
+
+          expires_at = user.send(token_expiration_date_attr)
+
+          !expires_at || (Time.now.in_time_zone < expires_at)
+        end
+
+        def token_response(options = {})
+          yield(options[:user], options[:failure]) if block_given?
+
+          options[:return_value]
         end
       end
     end

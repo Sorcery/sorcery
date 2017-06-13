31fcc44c083d5efe94c1e8dd1225022ab2c5d1b8
Dmitrii Topornin <dtopornin@gmail.com>
2017-06-13 22:01:33 +0300

    add description to jwt auth methods

--- lib/sorcery/controller/submodules/jwt_auth.rb
+++ lib/sorcery/controller/submodules/jwt_auth.rb
@@ -7,6 +7,7 @@ module Sorcery
         end
 
         module InstanceMethods
+          # This method return generated token if user can be authenticated
           def jwt_login(*credentials)
             user = user_class.authenticate(*credentials)
             if user
@@ -18,6 +19,7 @@ module Sorcery
             end
           end
 
+          # To be used as a before_action.
           def jwt_require_auth
             jwt_not_authenticated && return unless jwt_user_id
 
@@ -26,10 +28,13 @@ module Sorcery
             jwt_not_authenticated && return
           end
 
+          # This method creating JWT token by payload
           def jwt_encode(payload)
             JWT.encode(payload, Config.jwt_secret_key)
           end
 
+          # This method decoding JWT token
+          # Return nil if token incorrect
           def jwt_decode(token)
             HashWithIndifferentAccess.new(
               JWT.decode(token, Config.jwt_secret_key)[0]
@@ -38,18 +43,25 @@ module Sorcery
             nil
           end
 
+          # Take token from header, by key defined in config
+          # With memoization
           def jwt_from_header
             @jwt_header_token ||= request.headers[Config.jwt_headers_key]
           end
 
+          # Return user data which decoded from token
+          # With memoization
           def jwt_user_data(token = jwt_from_header)
             @jwt_user_data ||= jwt_decode(token)
           end
 
+          # Return user id from user data if id present.
+          # Else return nil
           def jwt_user_id
             jwt_user_data.try(:[], :id)
           end
 
+          # This method called if user not authenticated
           def jwt_not_authenticated
             respond_to do |format|
               format.html { not_authenticated }

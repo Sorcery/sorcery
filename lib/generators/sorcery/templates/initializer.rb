9e466cfdb5b01c4a2dbc4f3b1d89548d52f3d2bf
bealking <bealking@gmail.com>
2017-10-07 23:11:54 +0800

    bug fix

--- lib/generators/sorcery/templates/initializer.rb
+++ lib/generators/sorcery/templates/initializer.rb
@@ -161,6 +161,7 @@ Rails.application.config.sorcery.configure do |config|
   # config.qq.key = ""
   # config.qq.secret = ""
   # config.qq.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=qq"
+  # config.qq.user_info_mapping = {:username => "nickname"}
   #
   # config.google.key = ""
   # config.google.secret = ""

72a9306b6d094e7f9fb0bc61efe7621b91bb14a7
Dzmitry <dzmitry.kovalenok@thehamon.com>
2018-02-07 13:07:12 +0300

    Extract authorize with vk request stub into a method

--- spec/providers/vk_spec.rb
+++ spec/providers/vk_spec.rb
@@ -15,14 +15,18 @@ describe Sorcery::Providers::Vk do
     sorcery_controller_external_property_set(:vk, :secret, "SECRET")
   end
 
+  def stub_vk_authorize
+    stub_request(:post, /https\:\/\/oauth\.vk\.com\/access_token/)
+      .to_return(
+        status: 200,
+        body: '{"access_token":"TOKEN","expires_in":86329,"user_id":1}',
+        headers: {'content-type' => 'application/json'})
+  end
+
   context "getting user info hash" do
     it "should provide VK API version" do
+      stub_vk_authorize
       sorcery_controller_external_property_set(:vk, :api_version, '5.71')
-      stub_request(:post, /https\:\/\/oauth\.vk\.com\/access_token/)
-        .to_return(
-           status: 200,
-           body: '{"access_token":"TOKEN","expires_in":86329,"user_id":1}',
-           headers: {'content-type' => 'application/json'})
 
       get_user = stub_request(:get, "https://api.vk.com/method/getProfiles?access_token=TOKEN&fields=&scope=email&uids=1&v=5.71")
         .to_return(body: '{"response":[{"id":1}]}')

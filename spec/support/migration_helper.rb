eb0efbbdebc40553ff391159aa8adaa0c28a4f7a
Akhil Naini <akhil@seneca.systems>
2018-06-13 18:07:06 -0700

    Fix specs for Rails 4

--- spec/support/migration_helper.rb
+++ spec/support/migration_helper.rb
@@ -1,18 +1,18 @@
 class MigrationHelper
   class << self
     def migrate(path)
-      if Rails.gem_version >= Gem::Version.new('5.2.0')
+      if ActiveRecord.version >= Gem::Version.new('5.2.0')
         ActiveRecord::MigrationContext.new(path).migrate
       else
-        MigrationHelper.migrate(path)
+        ActiveRecord::Migrator.migrate(path)
       end
     end
 
     def rollback(path)
-      if Rails.gem_version >= Gem::Version.new('5.2.0')
+      if ActiveRecord.version >= Gem::Version.new('5.2.0')
         ActiveRecord::MigrationContext.new(path).rollback
       else
-        MigrationHelper.rollback(path)
+        ActiveRecord::Migrator.rollback(path)
       end
     end
   end

786835ff94cb11ca3597dd6a7772550782e2a2d2
Valentine C <valentine.git@me.vc.sg>
2018-10-20 06:00:07 +0800

    Add magic_login to initializer comments

--- lib/generators/sorcery/templates/initializer.rb
+++ lib/generators/sorcery/templates/initializer.rb
@@ -1,7 +1,8 @@
 # The first thing you need to configure is which modules you need in your app.
 # The default is nothing which will include only core features (password encryption, login/logout).
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
-# :reset_password, :session_timeout, :brute_force_protection, :activity_logging, :external
+# :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
+# :magic_login, :external
 Rails.application.config.sorcery.submodules = []
 
 # Here you can configure each submodule's features.

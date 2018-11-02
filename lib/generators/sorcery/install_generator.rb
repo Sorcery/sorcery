f58082f49462692604142fb35251d642bfb17799
Joshua Buker <buker.joshua@gmail.com>
2018-11-01 17:52:16 -0700

    Fix Style/FormatString

--- lib/generators/sorcery/install_generator.rb
+++ lib/generators/sorcery/install_generator.rb
@@ -79,7 +79,7 @@ module Sorcery
           sleep 1 # make sure each time we get a different timestamp
           Time.new.utc.strftime('%Y%m%d%H%M%S')
         else
-          '%.3d' % (current_migration_number(dirname) + 1)
+          format('%.3d', (current_migration_number(dirname) + 1))
         end
       end
 

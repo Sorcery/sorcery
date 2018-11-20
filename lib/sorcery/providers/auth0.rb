0368032f46f547be741ad438e34b9a0e7f863384
Joshua Buker <buker.joshua@gmail.com>
2018-11-20 10:10:31 -0800

    Add Auth0 provider to external
    Used github/google providers as reference, as well as the Omniauth/Auth0
    strategy.

---
+++ lib/sorcery/providers/auth0.rb
@@ -0,0 +1,55 @@
+module Sorcery
+  module Providers
+    # This class adds support for OAuth with Auth0.com
+    #
+    #   config.auth0.key = <key>
+    #   config.auth0.secret = <secret>
+    #   config.auth0.domain = <domain>
+    #   ...
+    #
+    class Auth0 < Base
+      include Protocols::Oauth2
+
+      attr_accessor :auth_path, :token_path, :user_info_path, :domain
+
+      def initialize
+        super
+
+        @auth_path = '/authorize'
+        @token_path = '/oauth/token'
+        @user_info_path = '/userinfo'
+      end
+
+      def site
+        site = URI(@domain)
+        site = URI("https://#{site}") if site.scheme.nil?
+        site.to_s
+      end
+
+      def site=(domain)
+        @domain = domain
+      end
+
+      def get_user_hash(access_token)
+        response = access_token.get(user_info_path)
+
+        auth_hash(access_token).tap do |h|
+          h[:user_info] = JSON.parse(response.body)
+          h[:uid] = h[:user_info]['user_id']
+        end
+      end
+
+      def login_url(_params, _session)
+        authorize_url(authorize_url: auth_path)
+      end
+
+      def process_callback(params, _session)
+        args = {}.tap do |a|
+          a[:code] = params[:code] if params[:code]
+        end
+
+        get_access_token(args, token_url: token_path, token_method: :post)
+      end
+    end
+  end
+end

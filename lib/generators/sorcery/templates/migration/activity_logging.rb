ebb483a31ca1d01d0ef61561617c9d5d625d76a0
Mladen Ilić <mladenilic480@gmail.com>
2019-01-25 17:59:01 +0100

    Update migration templates to use new hash syntax (#170)

--- lib/generators/sorcery/templates/migration/activity_logging.rb
+++ lib/generators/sorcery/templates/migration/activity_logging.rb
@@ -1,9 +1,9 @@
 class SorceryActivityLogging < <%= migration_class_name %>
   def change
-    add_column :<%= model_class_name.tableize %>, :last_login_at,     :datetime, :default => nil
-    add_column :<%= model_class_name.tableize %>, :last_logout_at,    :datetime, :default => nil
-    add_column :<%= model_class_name.tableize %>, :last_activity_at,  :datetime, :default => nil
-    add_column :<%= model_class_name.tableize %>, :last_login_from_ip_address, :string, :default => nil
+    add_column :<%= model_class_name.tableize %>, :last_login_at,     :datetime, default: nil
+    add_column :<%= model_class_name.tableize %>, :last_logout_at,    :datetime, default: nil
+    add_column :<%= model_class_name.tableize %>, :last_activity_at,  :datetime, default: nil
+    add_column :<%= model_class_name.tableize %>, :last_login_from_ip_address, :string, default: nil
 
     add_index :<%= model_class_name.tableize %>, [:last_logout_at, :last_activity_at]
   end

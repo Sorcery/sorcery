f67b9895a5816c188e40c16d2ab72e1bc8d94134
Ryusuke Tomita <sukesukeryu@users.noreply.github.com>
2019-05-29 03:49:07 +0900

    ignore cookies when undefined cookies (#187)

--- lib/sorcery/controller/submodules/remember_me.rb
+++ lib/sorcery/controller/submodules/remember_me.rb
@@ -59,7 +59,7 @@ module Sorcery
           # and logs the user in if found.
           # Runs as a login source. See 'current_user' method for how it is used.
           def login_from_cookie
-            user = cookies.signed[:remember_me_token] && user_class.sorcery_adapter.find_by_remember_me_token(cookies.signed[:remember_me_token])
+            user = cookies.signed[:remember_me_token] && user_class.sorcery_adapter.find_by_remember_me_token(cookies.signed[:remember_me_token]) if defined? cookies
             if user && user.has_remember_me_token?
               set_remember_me_cookie!(user)
               session[:user_id] = user.id.to_s

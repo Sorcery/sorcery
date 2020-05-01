6b72ca36cf389804963e2553ccbb3da000518e51
Josh Buker <buker.joshua@gmail.com>
2020-05-01 18:32:28 +0000

    Revert on_load change due to breaking existing applications (#234)

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -7,13 +7,19 @@ module Sorcery
   class Engine < Rails::Engine
     config.sorcery = ::Sorcery::Controller::Config
 
+    # TODO: Should this include a modified version of the helper methods?
     initializer 'extend Controller with sorcery' do
-      # TODO: Should this include a modified version of the helper methods?
-      ActiveSupport.on_load(:action_controller_api) do
+      # FIXME: on_load is needed to fix Rails 6 deprecations, but it breaks
+      #        applications due to undefined method errors.
+      # ActiveSupport.on_load(:action_controller_api) do
+      if defined?(ActionController::API)
         ActionController::API.send(:include, Sorcery::Controller)
       end
 
-      ActiveSupport.on_load(:action_controller_base) do
+      # FIXME: on_load is needed to fix Rails 6 deprecations, but it breaks
+      #        applications due to undefined method errors.
+      # ActiveSupport.on_load(:action_controller_base) do
+      if defined?(ActionController::Base)
         ActionController::Base.send(:include, Sorcery::Controller)
         ActionController::Base.helper_method :current_user
         ActionController::Base.helper_method :logged_in?

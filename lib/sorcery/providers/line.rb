a6cef7e42d619dc45b46c9326705c8d4e1bd2b13
horiken <kentaro.horie.4@gmail.com>
2020-07-07 16:22:46 +0900

    Support line login v2.1 (#251)

--- lib/sorcery/providers/line.rb
+++ lib/sorcery/providers/line.rb
@@ -9,15 +9,16 @@ module Sorcery
     class Line < Base
       include Protocols::Oauth2
 
-      attr_accessor :token_url, :user_info_path, :auth_path
+      attr_accessor :token_url, :user_info_path, :auth_path, :scope, :bot_prompt
 
       def initialize
         super
 
         @site           = 'https://access.line.me'
         @user_info_path = 'https://api.line.me/v2/profile'
-        @token_url      = 'https://api.line.me/v2/oauth/accessToken'
-        @auth_path      = 'dialog/oauth/weblogin'
+        @token_url      = 'https://api.line.me/oauth2/v2.1/token'
+        @auth_path      = 'oauth2/v2.1/authorize'
+        @scope          = 'profile'
       end
 
       def get_user_hash(access_token)
@@ -34,13 +35,28 @@ module Sorcery
         @state = SecureRandom.hex(16)
         authorize_url(authorize_url: auth_path)
       end
+
+      # overrides oauth2#authorize_url to add bot_prompt query.
+      def authorize_url(options = {})
+        options.merge!({
+          connection_opts: { params: { bot_prompt: bot_prompt } }
+        }) if bot_prompt.present?
+
+        super(options)
+      end
+
       # tries to login the user from access token
       def process_callback(params, _session)
         args = {}.tap do |a|
           a[:code] = params[:code] if params[:code]
         end
 
-        get_access_token(args, token_url: token_url, token_method: :post)
+        get_access_token(
+          args,
+          token_url: token_url,
+          token_method: :post,
+          grant_type: 'authorization_code'
+        )
       end
     end
   end

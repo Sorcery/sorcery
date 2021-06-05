107a5473a7d4b86beb55f90809b3d2980c1249ea
Mladen Ilić <mladenilic480@gmail.com>
2021-06-05 02:39:35 +0200

    Inline index definition in core migration (#281)

--- lib/generators/sorcery/templates/migration/core.rb
+++ lib/generators/sorcery/templates/migration/core.rb
@@ -1,13 +1,11 @@
 class SorceryCore < <%= migration_class_name %>
   def change
     create_table :<%= tableized_model_class %> do |t|
-      t.string :email,            null: false
+      t.string :email,            null: false, index: { unique: true }
       t.string :crypted_password
       t.string :salt
 
       t.timestamps                null: false
     end
-
-    add_index :<%= tableized_model_class %>, :email, unique: true
   end
 end

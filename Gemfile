7b8e3bd3a30c4d92118733245e1e97c1f75980a1
Andy Reilly <andy@firedatasolutions.com>
2025-01-22 13:07:21 -0800

    added gemfiles for Rails 7.2 and 8.0 and all tests passing with ruby 3.2.4

--- Gemfile
+++ Gemfile
@@ -3,6 +3,4 @@ source 'https://rubygems.org'
 gem 'pry'
 gem 'rails'
 gem 'rails-controller-testing'
-gem 'sqlite3', '~> 1.0'
-
 gemspec
--- spec/support/migration_helper.rb
+++ spec/support/migration_helper.rb
@@ -1,17 +1,19 @@
 class MigrationHelper
   class << self
     def migrate(path)
-      ActiveRecord::MigrationContext.new(path, schema_migration).migrate
+      ActiveRecord::MigrationContext.new(path).migrate
     end
 
     def rollback(path)
-      ActiveRecord::MigrationContext.new(path, schema_migration).rollback
+      ActiveRecord::MigrationContext.new(path).rollback
     end
 
-    private
-
-    def schema_migration
-      ActiveRecord::Base.connection.schema_migration
-    end
+    # private
+    # Commenting out as this is not needed. Was causing error due to deprecation of .connection superseded by
+    # .with_connection. Removed schema_migration from new(path, schema_migrations) since it is optional
+    # and all test now passing with rails 7.2.
+    # def schema_migration
+    #  ActiveRecord::Base.connection.schema_migration
+    # end
   end
 end

ba4ee58891b4ac87b580fd7082112650dadd139c
Andrew M <beeengine@gmail.com>
2025-10-24 12:05:08 +0300

    Fix Lint/SendWithMixinArgument

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -17,14 +17,14 @@ module Sorcery
       #        applications due to undefined method errors.
       # ActiveSupport.on_load(:action_controller_api) do
       if defined?(ActionController::API)
-        ActionController::API.send(:include, Sorcery::Controller)
+        ActionController::API.include Sorcery::Controller
       end
 
       # FIXME: on_load is needed to fix Rails 6 deprecations, but it breaks
       #        applications due to undefined method errors.
       # ActiveSupport.on_load(:action_controller_base) do
       if defined?(ActionController::Base)
-        ActionController::Base.send(:include, Sorcery::Controller)
+        ActionController::Base.include Sorcery::Controller
         ActionController::Base.helper_method :current_user
         ActionController::Base.helper_method :logged_in?
       end
